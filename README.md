a9e4698cf1df68e4086d53832f7be3bbdda77b36
Romilly Cocking <romilly.cocking@gmail.com>
2019-05-14 10:51:00 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Getting Started with the Jetson Nano
+
+This is the repository for my blog series *Getting Started with the Jetson Nano*.
+
+It contains a setup script and Jupyter notebooks.
+
+At present, documentation for these resources is on my blog.
+
+1. [Getting Started with the Jetson Nano](https://blog.rareschool.com/2019/05/getting-started-with-jetson-nano.html)
+1. Getting Started - Part 1.5 is now out-of-date: ignore it!
+1. [Getting Started with the Jetson Nano - Part 2](https://blog.rareschool.com/2019/05/getting-started-with-jetson-nano-part-2.html) covers the initial boot and Ubuntu configuration.
+1. Getting Started with the Jetson Nano - Part 3 will cover installing TensorFlow and other prerequisites, and lead you through training and testing a complete example.
+
+I'll also be covering model import and transfer learning.
+
+The Jupyter notebooks are based on existing examples;
+they contain details of the notebooks from which they were derived.
+
---
+++ nano-tf-01.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# make executable by chmod a+x nano-tf-01.sh
+# run using sudo -H ./nano-tf-01.sh
+# first install the Ubuntu packages we need
+apt-get install python3-pip libhdf5-serial-dev hdf5-tools -y # needed for TensorFlow
+apt-get install libfreetype6-dev pkg-config -y               # needed for matplotlib
+apt-get install nodejs npm -y                                # needed for jupyter lab
+pip3 install pillow==5.4.1                                   # needed for image processing
+pip3 install matplotlib                                      # used in the TensorFlow notebook
+# Jupyter  notebook - lab + widgets add terminal window support
+pip3 install jupyter jupyterlab
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter labextension install @jupyterlab/statusbar
+# and finally, official NVIDIA TensorFlow package for the Nano
+pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu==1.13.1+nv19.4
