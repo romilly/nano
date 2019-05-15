7928e397d3051cd960d4a99b3ccf4f3c259318a3
Romilly Cocking <romilly.cocking@gmail.com>
2019-05-15 15:45:38 +0100

    removed redundant sudo from install-service.sh

--- scripts/install-service.sh
+++ scripts/install-service.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo mv nano_jupyter.service /etc/systemd/system/nano_jupyter.service
-sudo systemctl enable nano_jupyter
-sudo systemctl start nano_jupyter
+mv nano_jupyter.service /etc/systemd/system/nano_jupyter.service
+systemctl enable nano_jupyter
+systemctl start nano_jupyter
